b67f4a37ca9be1e27e7cba8608f8cb0a61ba89d0
Seif-Sallam <Seif_Sallam@aucegypt.edu>
2021-03-21 02:33:34 +0200

    Changed the hierarchy

---
+++ KMapSolver/src/Engine/Application.cpp
@@ -0,0 +1,103 @@
+#include "Application.h"
+using namespace WI;
+
+float Application::m_fDTime = 0.0f;
+sf::Clock Application::m_appClock;
+sf::Color Application::m_clearColor;
+States Application::m_eAppState;
+
+WI::Application::Application()
+{
+	m_appWindow = new sf::RenderWindow(sf::VideoMode(800u, 600u), "APP");
+	m_event = new sf::Event;
+	m_appWindow->setView(m_defaultView);
+	m_defaultView.setCenter(m_defaultView.getSize().x / 2.0f, m_defaultView.getSize().y / 2.0f);
+	m_eAppState = States::Main;
+	m_clearColor = sf::Color::Black;
+}
+
+Application::Application(const sf::Vector2u& resolution, const std::string& appName, const sf::Color& clearColor)
+{
+	m_appWindow = new sf::RenderWindow(sf::VideoMode(resolution.x, resolution.y), appName);
+	m_event = new sf::Event;
+	m_appWindow->setView(m_defaultView);
+	m_defaultView.setCenter(m_defaultView.getSize().x / 2.0f, m_defaultView.getSize().y / 2.0f);
+	m_eAppState = States::Main;
+	m_clearColor = clearColor;
+}
+
+uint32_t Application::GetScreenWidth()
+{
+	return uint32_t(m_appWindow->getSize().x);
+}
+
+uint32_t Application::GetScreenHeight()
+{
+	return uint32_t(m_appWindow->getSize().y);
+}
+
+void Application::HandleEvents_()
+{
+	while (m_appWindow->pollEvent(*m_event))
+	{
+		if (m_event->type == sf::Event::Closed)
+			m_eAppState = States::Exit;
+		/*if (m_event->type == sf::Event::Resized)
+		{
+			m_defaultView.setSize(sf::Vector2f(m_appWindow->getSize()));
+			std::cout << "Here\n";
+		}*/
+		HandleEvents();
+	}
+}
+void Application::HandleEvents()
+{
+
+}
+
+void Application::Update()
+{
+}
+
+void Application::Draw()
+{
+}
+
+void Application::CheckStates()
+{
+}
+
+void Application::CheckOGStates()
+{
+	if (m_eAppState == States::Exit)
+		m_appWindow->close();
+}
+
+void Application::Run()
+{
+	while (m_appWindow->isOpen())
+	{
+		m_fDTime = m_appClock.restart().asSeconds();
+		HandleEvents_();
+		CheckStates();
+		CheckOGStates();
+		Update();
+		
+		m_appWindow->setView(m_defaultView);
+		m_appWindow->clear(m_clearColor);
+		Draw();
+		m_appWindow->display();
+	}
+}
+void Application::SetClearColor(const sf::Color& clearColor)
+{
+	m_clearColor = clearColor;
+}
+
+Application::~Application()
+{
+	if (m_appWindow != nullptr)
+		delete m_appWindow;
+	if (m_event != nullptr)
+		delete m_event;
+}
---
+++ KMapSolver/src/Engine/Button.cpp
@@ -0,0 +1,142 @@
+#include "Button.h"
+using namespace WI;
+
+
+Button::Button(sf::RenderWindow* window, ButtonConfig config)
+	: m_window(window)
+{
+	this->config = config;
+	AddConfig(config);
+	m_text.setCharacterSize(16u);
+	sf::FloatRect bounds = m_text.getLocalBounds();
+	m_body.setSize(sf::Vector2f(bounds.width + 15.0f, bounds.height + 15.0f));
+	m_size = m_body.getSize();
+	m_bHasStaticSize = false;
+}
+
+bool Button::EventHandling(sf::Event* event, const std::string& newText, bool reAdjust, void(*function)(Button*) )
+{
+	if(MouseOnButton())
+		if (event->type == sf::Event::MouseButtonPressed && event->mouseButton.button == sf::Mouse::Left)
+		{
+			Flicker(true);
+			function(this);
+			if(newText != "")
+				UpdateText(newText, reAdjust);
+			return true;
+		}
+		else
+			Flicker(false);
+	return false;
+		
+}
+
+void Button::UpdateText(const std::string& text, bool reAdjust)
+{
+	m_text.setString(text);
+	if (reAdjust)
+	{
+		sf::FloatRect bounds = m_text.getLocalBounds();
+		m_body.setSize(sf::Vector2f(bounds.width + 15.0f, bounds.height + 25.0f));
+		m_size = m_body.getSize();
+	}
+}
+
+std::string Button::GetButtonText()
+{
+	return m_text.getString();
+}
+
+void Button::SetTextSize(const unsigned int& size)
+{
+	m_text.setCharacterSize(size);
+}
+
+void Button::SetStaticSize(const sf::Vector2f& size)
+{
+	m_bHasStaticSize = true;
+	m_body.setSize(size);
+	m_size = size;
+	float textWidth = m_text.getLocalBounds().width;
+	float textHeight = m_text.getLocalBounds().height;
+
+	m_text.setPosition(m_position.x + textWidth / 4.0f, m_position.y + size.y / 2.0f - textHeight / 2.0f);
+	m_position = m_body.getPosition();
+}
+
+void Button::SetFont(const sf::Font& font)
+{
+	m_text.setFont(font);
+}
+
+void Button::SetPosition(const sf::Vector2f& position)
+{
+	m_position = position;
+	m_body.setPosition(position.x, position.y + 2.0f);
+	if(!m_bHasStaticSize)
+		m_text.setPosition(position.x + 5.0f, position.y + 5.0f);
+	else
+	{
+		float textWidth = m_text.getLocalBounds().width;
+		float textHeight = m_text.getLocalBounds().height;
+		m_text.setPosition(m_position.x + textWidth / 4.0f, m_position.y + m_body.getSize().y / 2.0f - textHeight / 2.0f);
+	}
+}
+
+sf::Vector2f Button::GetSize() const
+{
+	return m_body.getSize();
+}
+
+void Button::WrapToTopLeftCorner(int index)
+{
+	sf::Vector2f size = sf::Vector2f(m_window->getView().getSize().x, m_window->getView().getSize().y);
+	sf::Vector2f pos = sf::Vector2f(m_window->getView().getCenter().x - size.x / 2.0f, m_window->getView().getCenter().y - size.y / 2.0f);
+	
+	SetPosition(sf::Vector2f(pos.x + (m_body.getSize().x + 10.0f) * index, pos.y));
+}
+
+void Button::AddConfig(const ButtonConfig& config)
+{
+	if (config.transparent) 
+		m_body.setFillColor(sf::Color::Transparent);
+	else
+		m_body.setFillColor(config.bgColor);
+	if (config.hasBorders)
+	{
+		m_body.setOutlineThickness(config.brdrSize);
+		m_body.setOutlineColor(config.brdrColor);
+	}
+	else
+		m_body.setOutlineThickness(0.f);
+
+	if (config.font != nullptr)
+	{
+		m_text.setFont(*config.font);
+	}
+	m_text.setFillColor(config.txtColor);	
+	m_text.setString(config.text);
+}
+
+void Button::draw(sf::RenderTarget& window, sf::RenderStates state) const
+{
+	window.draw(m_body, state);
+	window.draw(m_text, state);
+}
+
+bool Button::MouseOnButton()
+{
+	sf::Vector2f mousePos = m_window->mapPixelToCoords(sf::Mouse::getPosition(*m_window));
+
+	if (mousePos.x < m_position.x + m_size.x && mousePos.x > m_position.x && mousePos.y < m_position.y + m_size.y && mousePos.y > m_position.y)
+		return true;
+	return false;
+}
+
+void Button::Flicker(bool flickIn)
+{
+	if (flickIn)
+		m_body.setFillColor(config.flickerColor);
+	else
+		m_body.setFillColor(config.bgColor);
+}
