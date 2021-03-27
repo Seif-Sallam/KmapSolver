a18b766c174deaaa45ad0fb131cd2455a8abeff6
Seif-Sallam <Seif_Sallam@aucegypt.edu>
2021-03-27 23:17:14 +0200

    Fixed some bugs

--- KMapSolver/src/Kmap.cpp
+++ KMapSolver/src/Kmap.cpp
@@ -189,11 +189,11 @@ void KM::Kmap::ReConfig()
 
 std::string KM::Kmap::Simplify()
 {
-	
+
 	//Check if they are all 1s and if yes you can just say the function is TRUE
 	int32_t trueCounter = 0;
 	int32_t falseCounter = 0;
-	for (uint32_t  i = 0; i < m_cellState.size(); i++)
+	for (uint32_t i = 0; i < m_cellState.size(); i++)
 	{
 		auto& state = m_cellState[i];
 		if (state == 1)
@@ -217,10 +217,10 @@ std::string KM::Kmap::Simplify()
 #endif // _DEBUG
 		return "F= False";
 	}
-	
+
 	//vector that will store the min terms as numbers
-	std::vector<uint16_t> minTerms; 
-	
+	std::vector<uint16_t> minTerms;
+
 	//Getting the min terms from the cell states
 	for (uint32_t i = 0; i < m_cellState.size(); i++)
 		if (m_cellState[i] == 1)
@@ -230,15 +230,10 @@ std::string KM::Kmap::Simplify()
 
 	//Convert to strings using bit sets
 
-	//The struct is just to make it easier for the algorithm to simplify the expressions
-	struct mTerm {
-		std::string number;
-		bool included = false;
-	};
 
+	std::vector<std::vector<uint32_t>> minTermsCovered(numberOfMinTerms);
 	//The vector that will store the min terms throughout the algorithm
 	std::vector<mTerm> minTermsInStrings(numberOfMinTerms);
-	
 	//Switch statement to know which number of bits to use to represent the numbers
 	//since we cannot use a variable in a template class
 	switch (numberOfVariables)
@@ -264,16 +259,20 @@ std::string KM::Kmap::Simplify()
 		for (int32_t i = 0; i < numberOfMinTerms; i++) {
 			std::bitset<4> bits = minTerms[i];
 			minTermsInStrings[i].number = bits.to_string();
-		}	
+		}
 	}
 	break;
+	}
+	for (uint32_t i = 0; i < minTermsInStrings.size(); i++)
+	{
+		minTermsInStrings[i].coveredTerms.push_back(BtoD(stoi(minTermsInStrings[i].number)));
 	}
 	//NOW we have them as strings, we can now do the double checking
 	//Double Checking is just the name i gave to this nested for loop, it should check one element to the adjacent ones
 
 
 	//This is the vector that will contain the final answers
-	std::vector<std::string> sweepVec;
+	std::vector<mTerm> sweepVec;
 #ifdef _DEBUG
 
 	std::cout << "Before the loop:\n";
@@ -299,11 +298,16 @@ std::string KM::Kmap::Simplify()
 					// This function returns true if there is only ONE bit difference, and populates the index with the index of the changed number.
 					if (DifferentByOneBit(minTermsInStrings[i].number, minTermsInStrings[j].number, index))
 					{
-
 						// We push it back to the sweep vector
-						sweepVec.push_back(minTermsInStrings[i].number);
+						sweepVec.push_back(minTermsInStrings[i]);
 						// We then change the variable's different bit to something that indicates that it is changed here i used the -
-						sweepVec.back()[index] = '-';
+						sweepVec.back().number[index] = '-';
+						
+						// We add the covered minterms to the vector
+						for (uint32_t one = 0; one < minTermsInStrings[j].coveredTerms.size(); one++)
+						{
+							sweepVec.back().coveredTerms.push_back(minTermsInStrings[j].coveredTerms[one]);
+						}
 						// We make sure that we used this variable
 						used = true;
 						minTermsInStrings[j].included = true; // we mark the other variable as included in one of the min terms
@@ -313,7 +317,7 @@ std::string KM::Kmap::Simplify()
 			// If it happens that one of the variables is never used and never included, then it must be an implicant that cannot be combined with anyone
 			// So we have to add it to the sweep vector anyway
 			if (!minTermsInStrings[i].included && !used)
-				sweepVec.push_back(minTermsInStrings[i].number);
+				sweepVec.push_back(minTermsInStrings[i]);
 		}
 		// We clean the strings to be able to use the array next round
 		minTermsInStrings.clear();
@@ -324,10 +328,10 @@ std::string KM::Kmap::Simplify()
 		auto it = std::unique(sweepVec.begin(), sweepVec.end());
 		sweepVec.resize(std::distance(sweepVec.begin(), it));
 		// Here I am populating the vector of strings with the new elements that it should work on
-		for (uint32_t i = 0; i < sweepVec.size(); i++)
+		for (uint32_t j = 0; j < sweepVec.size(); j++)
 		{
-			mTerm term{ sweepVec[i], false };
-			minTermsInStrings.push_back(term);
+			minTermsInStrings.push_back(sweepVec[j]);
+			minTermsInStrings.back().included = false;
 		}
 
 		// Just for debugging
@@ -335,55 +339,120 @@ std::string KM::Kmap::Simplify()
 		std::cout << rounds << std::endl;
 		for (uint32_t i = 0; i < sweepVec.size(); i++)
 		{
-			std::cout << sweepVec[i] << " ";
+			std::cout << sweepVec[i].number << " ";
 		}
 		std::cout << "\n----------------------\n";
 #endif // _DEBUG
-		
+
 		//because I want the sweep vector to be cleaned each and every iteration, and i want to also leave it as is in the final one
-		if(rounds != numberOfVariables - 2)
+		if (rounds != numberOfVariables - 2)
 			sweepVec.clear();
-		
-	}	
+
+	}
+
+
 	//Removing the equivalent terms 
-	int smallerVariable = -1;
+	int32_t smallerVariable = -1;
 	for (uint16_t i = 0; i < sweepVec.size(); i++)
 		for (uint16_t j = i + 1; j < sweepVec.size(); j++)
-			if (IsEquivalent(sweepVec[i], sweepVec[j], smallerVariable))
+			if (IsEquivalent(sweepVec[i].number, sweepVec[j].number, smallerVariable))
 			{
 				int16_t removedIndex = (smallerVariable == 0) ? i : j;
 				sweepVec.erase(sweepVec.begin() + removedIndex);
 			}
 
+#ifdef	_DEBUG
+	std::cout << "min terms covered terms\n";
+	for (uint32_t i = 0; i < sweepVec.size(); i++)
+	{
+		std::cout << i << " " << sweepVec[i].number << "  :  ";
+		for (uint32_t j = 0; j < sweepVec[i].coveredTerms.size(); j++)
+		{
+			std::cout << sweepVec[i].coveredTerms[j] << " ";
+		}
+		std::cout << std::endl;
+	}
+#endif
+	//This is the part where we find the essential prime implicants and find which ones to print first
+	std::vector<uint32_t> occurances(minTerms.size());
+	bool* essentialOnes = new bool[minTerms.size()];
+	for (uint32_t i = 0; i < sweepVec.size(); i++)
+		essentialOnes[i] = false;
+
+
+	for (uint32_t column = 0; column < minTerms.size(); column++) {
+		int32_t index = -1;
+		occurances[column] = 0;
+		for (uint32_t i = 0; i < sweepVec.size(); i++)
+		{
+			for (uint32_t j = 0; j < sweepVec[i].coveredTerms.size(); j++)
+				if (sweepVec[i].coveredTerms[j] == minTerms[column])
+				{
+					index = i;
+					occurances[column]++;
+					break;
+				}
+		}
+
+		//If we find that they are in ONE PI, then they are essential
+		if (occurances[column] <= 1)
+		{
+#ifdef _DEBUG
+			std::cout << sweepVec[index].number << " ";
+			std::cout << "Marked essential: " << index << std::endl;
+#endif
+			essentialOnes[index] = true;
+		}
+	}
+#ifdef _DEBUG
+	std::cout << "Occurances: " << std::endl;
+	for (uint32_t i = 0; i < minTerms.size(); i++)
+	{
+		std::cout << occurances[i] << "  ";
+	}
+	std::cout << std::endl;
+#endif
+	std::vector<int> takenTerms;
+	//This is a special case to add other prime implicants even if they are not essential. 
+	for (uint32_t i = 0; i < minTerms.size(); i++)
+	{
+		for (uint32_t j = i; j < minTerms.size(); j++)
+		{
+			if (i != j && occurances[i] != 1 && occurances[j] != 1 && occurances[i] == occurances[j])
+			{
+				essentialOnes[i] = true;
+			}
+		}
+	}
+
+
+	//Printing the final result
+	std::string result = "F = ";
+	for (uint32_t i = 0; i < sweepVec.size(); i++)
+	{	
+		bool flag = false;
+		if (essentialOnes[i] == true)
+		{
+#ifdef _DEBUG
+			std::cout << "it is essential\n";
+#endif
+			std::string tempString = TermInVariables(sweepVec[i].number);
+			result += tempString;
+			flag = true;
+		}
+		if (flag && i != sweepVec.size() - 1)
+			result += " + ";
+	}
+
 #ifdef _DEBUG
 	std::cout << "outside " << std::endl;
 	for (uint32_t i = 0; i < sweepVec.size(); i++)
 	{
-		std::cout << sweepVec[i] << " ";
+		std::cout << sweepVec[i].number << " ";
 	}
 	std::cout << "\n----------------------\n";
 #endif // _DEBUG
-	//Converting the output to simplified expression
-	std::string result = "F= ";
-	for (uint32_t i = 0; i < sweepVec.size(); i++)
-	{
-		for (uint32_t j = 0; j < numberOfVariables; j++)
-		{
-			char bit = sweepVec[i][j];
-			if (bit != '-')
-			{
-				//This means it is equal to either 0 or 1
-				char character = char((int)'A' + (int)j);
-				result += character;
-				if (bit == '0')
-				{
-					result += '\'';
-				}
-			}
-		}
-		if(i != sweepVec.size() - 1)
-			result += " + ";
-	}
+	delete[] essentialOnes;
 	return result;
 }
 
@@ -405,7 +474,7 @@ KM::Kmap::~Kmap()
 	delete[] m_expressions;
 }
 
-bool KM::Kmap::IsEquivalent(const std::string& first, const std::string& second, int& deletingString)
+bool KM::Kmap::IsEquivalent(const std::string& first, const std::string& second, int32_t& deletingString)
 {
 	//Checking if the two literals are equivalent
 	int16_t length = first.length();
@@ -435,7 +504,42 @@ bool KM::Kmap::IsEquivalent(const std::string& first, const std::string& second,
 	return false;
 }
 
-bool KM::Kmap::DifferentByOneBit(const std::string& first, const std::string& second, int& index)
+//A generic Binary to decimal converter
+uint32_t KM::Kmap::BtoD(uint32_t n)
+{
+	uint32_t decimalNumber = 0;
+	uint32_t base = 1;
+	uint32_t temp = n;
+	while (temp) {
+		uint32_t lastDigit = temp % 10;
+		temp = temp / 10;
+		decimalNumber += lastDigit * base;
+		base = base * 2;
+	}
+	return decimalNumber;
+}
+
+std::string KM::Kmap::TermInVariables(const std::string& term)
+{
+	std::string result = "";
+	for (uint32_t j = 0; j < numberOfVariables; j++)
+	{
+		char bit = term[j];
+		if (bit != '-')
+		{
+			//This means it is equal to either 0 or 1
+			char character = char((uint32_t)'A' + (uint32_t)j);
+			result += character;
+			if (bit == '0')
+			{
+				result += '\'';
+			}
+		}
+	}
+	return result;
+}
+
+bool KM::Kmap::DifferentByOneBit(const std::string& first, const std::string& second, int32_t& index)
 {
 	//Knowing if the they are different by one bit or not
 	int32_t counter = 0;
--- KMapSolver/src/Kmap.h
+++ KMapSolver/src/Kmap.h
@@ -3,10 +3,51 @@
 #include <string>
 #include <vector>
 #include "Engine/Button.h"
+#include <iterator>
+#include <algorithm>
 #include <bitset>
 
 namespace KM {
-	
+	//The struct is just to make it easier for the algorithm to simplify the expressions
+	struct mTerm {
+		std::string number;
+		bool included = false;
+		std::vector<int> coveredTerms;
+		mTerm()
+		{
+			number = "";
+		}
+		mTerm(const mTerm& other)
+		{
+			this->number = other.number;
+			this->included = other.included;
+			for (int i = 0; i < other.coveredTerms.size(); i++)
+			{
+				this->coveredTerms.push_back(other.coveredTerms[i]);
+			}
+		}
+		//Those operators are there for std::sort and std::unique
+		bool operator==(const mTerm& other)
+		{
+			return this->number == other.number;
+		}
+		bool operator>=(const mTerm& other)
+		{
+			return this->number >= other.number;
+		}
+		bool operator<=(const mTerm& other)
+		{
+			return this->number <= other.number;
+		}
+		bool operator<(const mTerm& other)
+		{
+			return this->number < other.number;
+		}
+		bool operator>(const mTerm& other)
+		{
+			return this->number > other.number;
+		}
+	};
 	//Inheriting from sf::Drawable to be able to draw directly using sf::RenderWindow::draw();
 	class Kmap : public sf::Drawable
 	{
@@ -26,10 +67,15 @@ namespace KM {
 		~Kmap();
 	private:
 		//To know if two literals are equivalent or not
-		bool IsEquivalent(const std::string& first, const std::string& second, int& smallerString);
+		bool IsEquivalent(const std::string& first, const std::string& second, int32_t& smallerString);
 
+		//A generic Binray to decimal converter function
+		uint32_t BtoD(uint32_t term);
+
+		//A helper function to get Terms in variables 
+		std::string TermInVariables(const std::string& term);
 		//To know if two literals are different by one bit
-		bool DifferentByOneBit(const std::string& first, const std::string& second, int& index);
+		bool DifferentByOneBit(const std::string& first, const std::string& second, int32_t& index);
 		std::vector<uint16_t>& m_cellState;
 		sf::Text* m_cellText;
 		WI::Button** m_buttons;
@@ -38,5 +84,6 @@ namespace KM {
 		WI::ButtonConfig m_buttonConfig;
 		std::bitset<2>* m_expressions;
 		const uint32_t numberOfVariables;
+		bool usedArray[16]{ false };
 	};
 }
